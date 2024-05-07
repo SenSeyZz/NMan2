d7f0a4f7e6cee559aa84d2f8a89f806a75e70b11
Paul Frappier <paul.frappier@hotmail.fr>
2024-05-07 18:33:25 +0200

    solved submodule

---
+++ Front/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/Max":["static\u002Fchunks\u002Fpages\u002FMax.js"],"/Paul":["static\u002Fchunks\u002Fpages\u002FPaul.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FMax","\u002FPaul","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
